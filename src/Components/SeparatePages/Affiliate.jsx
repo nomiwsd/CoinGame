18bc0a34caccd1414596e327863bc1550ba72dae
Muhammad Nouman <110379750+nomiwsd@users.noreply.github.com>
2024-02-20 20:23:40 +0500

    ui fixed

--- src/Components/SeparatePages/Affiliate.jsx
+++ src/Components/SeparatePages/Affiliate.jsx
@@ -13,7 +13,6 @@ import { useState } from "react";
 import { LiaClipboardSolid } from "react-icons/lia";
 import { MdOutlineClose } from "react-icons/md";
 import Modal from "../Header/Modal";
-import dollarsign from '../../assets/dollar.png'
 import openbox from '../../assets/openbox.png'
 import closebox from '../../assets/closebox.png'
 export default function Affiliate() {
@@ -36,7 +35,7 @@ export default function Affiliate() {
                 <Sidebar open={sidebarOpen} />
             </div></div>
             <div className={`flex-1 overflow-hidden ${sidebarOpen || msgbarOpen ? "w-full" : "w-screen"}`}>
-            <Modal isOpen={isQuestModalOpen} onClose={closeQuestModal}>
+            <Modal isOpen={isQuestModalOpen} onClose={closeQuestModal} >
           <div className='flex justify-center items-center h-full max-w-[550px]'>
             <div className='w-full px-4 py-4 flex flex-col justify-center items-center'>
               <div className='flex justify-between items-center w-full'>
@@ -44,42 +43,42 @@ export default function Affiliate() {
                 <MdOutlineClose onClick={closeQuestModal} className='text-lg font-bold text-white' />
               </div>
               <p className="text-base font-normal text-white flex gap-1 my-2">
-                Total rewards: <img src={dollarsign} alt="" className="w-5 h-5" /> <span> $0.00 USD</span>
+                Total rewards: <img src={dollar} alt="" className="w-5 h-5" /> <span> $0.00 USD</span>
               </p>
               <div className="flex flex-col gap-2 w-full">
                 <div className="bg-thirdcl rounded-md px-2 py-2 w-full">
-                  <div className="flex justify-between items-center gap-3 ">
+                  <div className="flex flex-col md:flex-row justify-between items-center gap-3 ">
                     <div className="flex items-center gap-1"> 
                       <img src={closebox} alt="" className="w-20 h-16" />
                       <div className="flex flex-col">
                         <p className="text-sm font-normal text-white">Enable 2FA</p>
                         <p className="text-xs font-light text-[#99A4B099]">Enable two-factor authentication to strengthen account security.</p>
-                        <p className="text-[#FFDE01] text-base font-normal flex items-center gap-1 w-full pt-1 h-8"><img src={dollarsign} alt="" className="w-7 h-7" />  Earn 0.1 USD</p>
+                        <p className="text-[#FFDE01] text-base font-normal flex items-center gap-1 w-full pt-1 h-8"><img src={dollar} alt="" className="w-7 h-7" />  Earn 0.1 USD</p>
                       </div>
                     </div>
                     <button className="bg-[#6E34E2B2] rounded-md py-2 px-2 text-white w-28 h-12">Go To</button>
                   </div>
                 </div>
                 <div className="bg-thirdcl rounded-md px-2 py-2 w-full">
-                  <div className="flex justify-between items-center gap-3 ">
+                  <div className="flex flex-col md:flex-row justify-between items-center gap-3 ">
                     <div className="flex items-center gap-1">
                       <img src={openbox} alt="" className="w-20 h-16" />
                       <div className="flex flex-col">
                         <p className="text-sm font-normal text-white">Email verification</p>
                         <p className="text-xs font-light text-[#99A4B099]">Verify your email address.</p>
-                        <p className="text-[#FFDE01] text-base font-normal flex items-center gap-1 w-full pt-1 h-8"><img src={dollarsign} alt="" className="w-7 h-7" />  Earn 0.25 USD</p>
+                        <p className="text-[#FFDE01] text-base font-normal flex items-center gap-1 w-full pt-1 h-8"><img src={dollar} alt="" className="w-7 h-7" />  Earn 0.25 USD</p>
                       </div></div>
                     <button className="bg-btncl rounded-md py-2 px-2 text-white w-28 h-12">Claim $0.25</button>
                   </div>
                 </div>
                 <div className="bg-thirdcl rounded-md px-2 py-2 w-full">
-                  <div className="flex justify-between items-center gap-3 ">
+                  <div className="flex flex-col md:flex-row justify-between items-center gap-3 ">
                     <div className="flex items-center gap-1">
                       <img src={closebox} alt="" className="w-20 h-16" />
                       <div className="flex flex-col">
                         <p className="text-sm font-normal text-white">Upload your avatar</p>
                         <p className="text-xs font-light text-[#99A4B099]">Upload your photo as an avatar..</p>
-                        <p className="text-[#FFDE01] text-base font-normal flex items-center gap-1 w-full pt-1 h-8"><img src={dollarsign} alt="" className="w-7 h-7" />  Earn 0.1 USD</p>
+                        <p className="text-[#FFDE01] text-base font-normal flex items-center gap-1 w-full pt-1 h-8"><img src={dollar} alt="" className="w-7 h-7" />  Earn 0.1 USD</p>
                       </div>
                     </div>
                     <button className="bg-[#6E34E2B2] rounded-md py-2 px-2 text-white w-28 h-12">Go To</button>
@@ -208,9 +207,9 @@ export default function Affiliate() {
                 </div>
                 <Footer />
             </div>
-            <div className={`realtive ${msgbarOpen ? "w-full md:w-60" : ""}`}>  <div className={`fixed  inset-y-0 top-[70px] h-[500px] max-h-[600px] right-0 w-full z-50 bg-primarycl overflow-y-auto scroll-smooth transition-transform duration-300 transform ${msgbarOpen ? "-translate-x-0 md:translate-x-0" : "translate-x-full md:translate-x-0"}`}>
-                <MessageSect open={msgbarOpen} />
-            </div></div>
+            <div className={`realtive ${msgbarOpen ? "w-full md:w-60" : ""}`}>  <div className={`fixed  inset-y-0 top-[70px] right-0 z-50 bg-primarycl overflow-y-auto scroll-smooth transition-transform duration-300 transform ${msgbarOpen ? "-translate-x-0 md:translate-x-0  w-full md:w-60" : "translate-x-full md:translate-x-0"}`}>
+        <MessageSect open={msgbarOpen} />
+      </div></div>
             <div className="fixed bottom-0 right-0 left-0 z-40 flex md:hidden bg-primarycl max-h-20 px-2 py-2">
                 <div className="grid grid-cols-4 gap-2 w-full">
                     <div className="flex flex-col justify-center items-center gap-1" onClick={toggleSidebar}>
--- src/Components/SeparatePages/Bonus.jsx
+++ src/Components/SeparatePages/Bonus.jsx
@@ -17,7 +17,7 @@ import dollar from '../../assets/dollar.png'
 import openbox from '../../assets/openbox.png'
 import closebox from '../../assets/closebox.png'
 export default function Bonus() {
-    const { sidebarOpen, setSidebarOpen, msgbarOpen, setMsgbarOpen,isQuestModalOpen, closeQuestModal } = useSidebar();
+    const { sidebarOpen, setSidebarOpen, msgbarOpen, setMsgbarOpen, isQuestModalOpen, closeQuestModal } = useSidebar();
 
     const toggleSidebar = () => {
         setSidebarOpen(!sidebarOpen);
@@ -32,61 +32,61 @@ export default function Bonus() {
                 <Sidebar open={sidebarOpen} />
             </div></div>
             <div className={`flex-1 overflow-hidden ${sidebarOpen || msgbarOpen ? "w-full" : "w-screen"}`}>
-            <Modal isOpen={isQuestModalOpen} onClose={closeQuestModal}>
-          <div className='flex justify-center items-center h-full max-w-[550px]'>
-            <div className='w-full px-4 py-4 flex flex-col justify-center items-center'>
-              <div className='flex justify-between items-center w-full'>
-                <p className="text-lg font-normal text-white">Quests</p>
-                <MdOutlineClose onClick={closeQuestModal} className='text-lg font-bold text-white' />
-              </div>
-              <p className="text-base font-normal text-white flex gap-1 my-2">
-                Total rewards: <img src={dollar} alt="" className="w-5 h-5" /> <span> $0.00 USD</span>
-              </p>
-              <div className="flex flex-col gap-2 w-full">
-                <div className="bg-thirdcl rounded-md px-2 py-2 w-full">
-                  <div className="flex justify-between items-center gap-3 ">
-                    <div className="flex items-center gap-1"> 
-                      <img src={closebox} alt="" className="w-20 h-16" />
-                      <div className="flex flex-col">
-                        <p className="text-sm font-normal text-white">Enable 2FA</p>
-                        <p className="text-xs font-light text-[#99A4B099]">Enable two-factor authentication to strengthen account security.</p>
-                        <p className="text-[#FFDE01] text-base font-normal flex items-center gap-1 w-full pt-1 h-8"><img src={dollar} alt="" className="w-7 h-7" />  Earn 0.1 USD</p>
-                      </div>
-                    </div>
-                    <button className="bg-[#6E34E2B2] rounded-md py-2 px-2 text-white w-28 h-12">Go To</button>
-                  </div>
-                </div>
-                <div className="bg-thirdcl rounded-md px-2 py-2 w-full">
-                  <div className="flex justify-between items-center gap-3 ">
-                    <div className="flex items-center gap-1">
-                      <img src={openbox} alt="" className="w-20 h-16" />
-                      <div className="flex flex-col">
-                        <p className="text-sm font-normal text-white">Email verification</p>
-                        <p className="text-xs font-light text-[#99A4B099]">Verify your email address.</p>
-                        <p className="text-[#FFDE01] text-base font-normal flex items-center gap-1 w-full pt-1 h-8"><img src={dollar} alt="" className="w-7 h-7" />  Earn 0.25 USD</p>
-                      </div></div>
-                    <button className="bg-btncl rounded-md py-2 px-2 text-white w-28 h-12">Claim $0.25</button>
-                  </div>
-                </div>
-                <div className="bg-thirdcl rounded-md px-2 py-2 w-full">
-                  <div className="flex justify-between items-center gap-3 ">
-                    <div className="flex items-center gap-1">
-                      <img src={closebox} alt="" className="w-20 h-16" />
-                      <div className="flex flex-col">
-                        <p className="text-sm font-normal text-white">Upload your avatar</p>
-                        <p className="text-xs font-light text-[#99A4B099]">Upload your photo as an avatar..</p>
-                        <p className="text-[#FFDE01] text-base font-normal flex items-center gap-1 w-full pt-1 h-8"><img src={dollar} alt="" className="w-7 h-7" />  Earn 0.1 USD</p>
-                      </div>
-                    </div>
-                    <button className="bg-[#6E34E2B2] rounded-md py-2 px-2 text-white w-28 h-12">Go To</button>
-                  </div>
-                </div>
+                <Modal isOpen={isQuestModalOpen} onClose={closeQuestModal} >
+                    <div className='flex justify-center items-center h-full max-w-[550px]'>
+                        <div className='w-full px-4 py-4 flex flex-col justify-center items-center'>
+                            <div className='flex justify-between items-center w-full'>
+                                <p className="text-lg font-normal text-white">Quests</p>
+                                <MdOutlineClose onClick={closeQuestModal} className='text-lg font-bold text-white' />
+                            </div>
+                            <p className="text-base font-normal text-white flex gap-1 my-2">
+                                Total rewards: <img src={dollar} alt="" className="w-5 h-5" /> <span> $0.00 USD</span>
+                            </p>
+                            <div className="flex flex-col gap-2 w-full">
+                                <div className="bg-thirdcl rounded-md px-2 py-2 w-full">
+                                    <div className="flex flex-col md:flex-row justify-between items-center gap-3 ">
+                                        <div className="flex items-center gap-1">
+                                            <img src={closebox} alt="" className="w-20 h-16" />
+                                            <div className="flex flex-col">
+                                                <p className="text-sm font-normal text-white">Enable 2FA</p>
+                                                <p className="text-xs font-light text-[#99A4B099]">Enable two-factor authentication to strengthen account security.</p>
+                                                <p className="text-[#FFDE01] text-base font-normal flex items-center gap-1 w-full pt-1 h-8"><img src={dollar} alt="" className="w-7 h-7" />  Earn 0.1 USD</p>
+                                            </div>
+                                        </div>
+                                        <button className="bg-[#6E34E2B2] rounded-md py-2 px-2 text-white w-28 h-12">Go To</button>
+                                    </div>
+                                </div>
+                                <div className="bg-thirdcl rounded-md px-2 py-2 w-full">
+                                    <div className="flex flex-col md:flex-row justify-between items-center gap-3 ">
+                                        <div className="flex items-center gap-1">
+                                            <img src={openbox} alt="" className="w-20 h-16" />
+                                            <div className="flex flex-col">
+                                                <p className="text-sm font-normal text-white">Email verification</p>
+                                                <p className="text-xs font-light text-[#99A4B099]">Verify your email address.</p>
+                                                <p className="text-[#FFDE01] text-base font-normal flex items-center gap-1 w-full pt-1 h-8"><img src={dollar} alt="" className="w-7 h-7" />  Earn 0.25 USD</p>
+                                            </div></div>
+                                        <button className="bg-btncl rounded-md py-2 px-2 text-white w-28 h-12">Claim $0.25</button>
+                                    </div>
+                                </div>
+                                <div className="bg-thirdcl rounded-md px-2 py-2 w-full">
+                                    <div className="flex flex-col md:flex-row justify-between items-center gap-3 ">
+                                        <div className="flex items-center gap-1">
+                                            <img src={closebox} alt="" className="w-20 h-16" />
+                                            <div className="flex flex-col">
+                                                <p className="text-sm font-normal text-white">Upload your avatar</p>
+                                                <p className="text-xs font-light text-[#99A4B099]">Upload your photo as an avatar..</p>
+                                                <p className="text-[#FFDE01] text-base font-normal flex items-center gap-1 w-full pt-1 h-8"><img src={dollar} alt="" className="w-7 h-7" />  Earn 0.1 USD</p>
+                                            </div>
+                                        </div>
+                                        <button className="bg-[#6E34E2B2] rounded-md py-2 px-2 text-white w-28 h-12">Go To</button>
+                                    </div>
+                                </div>
 
-              </div>
-            </div>
-          </div>
+                            </div>
+                        </div>
+                    </div>
 
-        </Modal>
+                </Modal>
                 <div className="flex flex-col gap-5 py-10 px-6 min-h-[880px]">
                     <div className="flex flex-col md:flex-row justify-between items-center bg-primarycl px-2 py-2 rounded-md">
                         <div className="flex flex-col md:flex-row justify-between items-center w-full md:w-2/4">
@@ -169,7 +169,7 @@ export default function Bonus() {
                                 <p className="text-xs font-normal text-[#8795A8]">Details</p>
                             </div>
                             <div className="flex justify-center items-center">
-                                <img src={Bonus2} alt=""  className="w-24 h-24" />
+                                <img src={Bonus2} alt="" className="w-24 h-24" />
                             </div>
                             <div className="flex flex-col justify-between items-center gap-1 w-full">
                                 <p className="text-xs font-normal text-[#8795A8]">Claim in</p>
@@ -211,11 +211,11 @@ export default function Bonus() {
                                 <div className="flex flex-col gap-1">
                                     <p className="text-white text-sm font-normal">Quests</p>
                                 </div>
-                           </div>
+                            </div>
                             <div className="flex justify-center items-cnter">
                                 <img src={Bonus5} alt="" className="w-24 h-24" />
                             </div>
-                           <div className="flex flex-col justify-between items-center gap-1 w-full">
+                            <div className="flex flex-col justify-between items-center gap-1 w-full">
                                 <button className="px-3 py-2 bg-btncl text-white w-20 h-10 rounded-md">Open</button>
                             </div>
                         </div>
@@ -223,7 +223,7 @@ export default function Bonus() {
                 </div>
                 <Footer />
             </div>
-            <div className={`realtive ${msgbarOpen ? "w-full md:w-60" : ""}`}>  <div className={`fixed  inset-y-0 top-[70px] h-[500px] max-h-[600px] right-0  w-full z-50 bg-primarycl overflow-y-auto scroll-smooth transition-transform duration-300 transform ${msgbarOpen ? "-translate-x-0 md:translate-x-0" : "translate-x-full md:translate-x-0"}`}>
+            <div className={`realtive ${msgbarOpen ? "w-full md:w-60" : ""}`}>  <div className={`fixed  inset-y-0 top-[70px] right-0 z-50 bg-primarycl overflow-y-auto scroll-smooth transition-transform duration-300 transform ${msgbarOpen ? "-translate-x-0 md:translate-x-0  w-full md:w-60" : "translate-x-full md:translate-x-0"}`}>
                 <MessageSect open={msgbarOpen} />
             </div></div>
             <div className="fixed bottom-0 right-0 left-0 z-40 flex md:hidden bg-primarycl max-h-20 px-2 py-2">
